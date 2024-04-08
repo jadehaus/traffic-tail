e3891bddaf291ca7bf70cb57b82060eeb7448b49
Minu Kim <minu.kim@kaist.ac.kr>
2024-04-08 17:47:36 +0900

    initial commit

---
+++ environment.py
@@ -0,0 +1,61 @@
+import traci
+from sumo_rl import SumoEnvironment
+
+'''
+Speed Mode
+bit0: Regard safe speed
+bit1: Regard maximum acceleration
+bit2: Regard maximum deceleration
+bit3: Regard right of way at intersections (only applies to approaching foe vehicles outside the intersection)
+bit4: Brake hard to avoid passing a red light
+bit5: Disregard right of way within intersections (only applies to foe vehicles that have entered the intersection).
+Setting the bit enables the check (the according value is regarded), keeping the bit==zero disables the check.
+
+Examples:
+default (all checks on) -> [0 1 1 1 1 1] -> Speed Mode = 31
+most checks off (legacy) -> [0 0 0 0 0 0] -> Speed Mode = 0
+all checks off -> [1 0 0 0 0 0] -> Speed Mode = 32
+disable right of way check -> [1 1 0 1 1 1] -> Speed Mode = 55
+run a red light [0 0 0 1 1 1] = 7 (also requires setSpeed or slowDown)
+run a red light even if the intersection is occupied [1 0 0 1 1 1] = 39 (also requires setSpeed or slowDown)
+'''
+
+
+class TailGatingEnv(SumoEnvironment):
+    def __init__(self, tailgating=True, *args, **kwargs):
+        super(TailGatingEnv, self).__init__(*args, **kwargs)
+        self.tailgating = tailgating
+                        
+    def _adjust_speed_mode(self, default_mode=31):
+        for tlsID in self.sumo.trafficlight.getIDList():
+            controlledLanes = self.sumo.trafficlight.getControlledLanes(tlsID)
+            stateString = self.sumo.trafficlight.getRedYellowGreenState(tlsID)
+            
+            for idx, lane in enumerate(controlledLanes):
+                vehicles = self.sumo.lane.getLastStepVehicleIDs(lane)
+                for vehID in vehicles:
+                    if 'y' in stateString[idx]:
+                        self.sumo.vehicle.setSpeedMode(vehID, 0)
+                    elif 'G' in stateString[idx]:
+                        self.sumo.vehicle.setSpeedMode(vehID, 7)
+                    else:
+                        self.sumo.vehicle.setSpeedMode(vehID, default_mode)
+
+    def _sumo_step(self):
+        if self.tailgating:
+            self._adjust_speed_mode()
+        self.sumo.simulationStep()
+        
+
+def create_env(tailgating=False):
+    return TailGatingEnv(
+        tailgating=tailgating,
+        net_file="nets/network.net.xml",
+        route_file="nets/flow.rou.xml",
+        single_agent=False,
+        use_gui=True,
+        num_seconds=86400,
+        yellow_time=3,
+        min_green=5,
+        max_green=60,
+    )
---
+++ trainer.py
@@ -0,0 +1,86 @@
+import os
+from argparse import ArgumentParser
+
+from linear_rl.true_online_sarsa import TrueOnlineSarsaLambda
+
+from environment import TailGatingEnv
+
+
+class Trainer(object):
+    """
+    Main training code.
+    Train a DQN model for each module in the environment.
+    """
+    def __init__(self, env='default'):
+        self.result_dir = f"results/{env}"
+        
+        if env == 'default':
+            tailgating = False
+        elif env == 'tailgating':
+            tailgating = True
+        else:
+            raise ValueError(f"Invalid environment {env}")
+        
+        self.env = TailGatingEnv(
+            tailgating=tailgating,
+            net_file="nets/network.net.xml",
+            route_file="nets/flow.rou.xml",
+            single_agent=False,
+            out_csv_name=self.result_dir,
+            use_gui=False,
+            num_seconds=86400,
+            yellow_time=3,
+            min_green=5,
+            max_green=60,
+        )   
+        
+        self.agents = {
+            ts_id: TrueOnlineSarsaLambda(
+                self.env.observation_spaces(ts_id),
+                self.env.action_spaces(ts_id),
+                alpha=0.000000001,
+                gamma=0.95,
+                epsilon=0.05,
+                lamb=0.1,
+                fourier_order=7,
+            )
+            for ts_id in self.env.ts_ids
+        }
+    
+    def train(self, episodes=1):
+        state = self.env.reset()
+        done = {"__all__": False}
+        
+        for _ in range(episodes):
+            while not done["__all__"]:
+                actions = {
+                    ts_id: self.agents[ts_id].act(state[ts_id]) 
+                    for ts_id in state.keys()
+                }
+                
+                next_state, reward, done, _ = self.env.step(action=actions)
+
+                for ts_id in next_state.keys():
+                    self.agents[ts_id].learn(
+                        state=next_state[ts_id], 
+                        action=actions[ts_id], 
+                        reward=reward[ts_id], 
+                        next_state=next_state[ts_id], 
+                        done=done[ts_id]
+                    )
+                    state[ts_id] = next_state[ts_id]
+                
+        self.env.save_csv(self.result_dir)
+        self.env.close()
+        return self.agents
+
+
+parser = ArgumentParser()
+parser.add_argument('--use-gui', action='store_true', default=False)
+parser.add_argument('--env', type=str, default='default')
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    trainer = Trainer(env=args.env)
+    trainer.train()
